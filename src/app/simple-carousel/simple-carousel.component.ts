359658376b92d6b69c09a9ad4f329ae4f15c877b
Jeroenouw <jeroenouw@hotmail.com>
2017-06-23 12:12:17 +0200

    Simple carousel component generated

---
+++ src/app/simple-carousel/simple-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-carousel',
+  templateUrl: './simple-carousel.component.html',
+  styleUrls: ['./simple-carousel.component.scss']
+})
+export class SimpleCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
